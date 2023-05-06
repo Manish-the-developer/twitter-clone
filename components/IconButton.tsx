6393ef438e87fe7532f784a23be0183b94e5094f
Manish Sharma <manish.sh1985@gmail.com>
2023-05-07 00:47:36 +0530

    Tweet component completed with icon button component in footer

---
+++ components/IconButton.tsx
@@ -0,0 +1,22 @@
+
+import { Text, View } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons'; 
+
+
+type IconButtonProps = {
+    icon : React.ComponentProps<typeof EvilIcons>['name']; //Type of evil icons
+    text ?: string | number;
+}
+
+const IconButton = ({icon, text} : IconButtonProps) => {
+    return (
+    <View style={{flexDirection:'row', alignItems:'center'}}>
+        {/* Icon */}
+        <EvilIcons name={icon} size={22} color="gray"/>
+        {/* Number */}
+        <Text style={{fontSize:12, color:'gray'}}>{text}</Text>
+    </View>
+    )
+}
+
+export default IconButton
