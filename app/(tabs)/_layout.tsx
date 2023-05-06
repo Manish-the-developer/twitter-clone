f34a751565dbe70540e9902fd70a115f7413772a
Manish Sharma <manish.sh1985@gmail.com>
2023-05-06 23:56:43 +0530

    Used flatlist to shown the tweets and added type for tweet component props

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One --updated',
+          title: 'Tab One',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
